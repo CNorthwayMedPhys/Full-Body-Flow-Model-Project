5f6852b107713b06600f5b7f0207c73daa770db6
Cassidy Northway <cbnorthway@gmail.com>
2023-12-20 12:29:42 -0800

    Simulation Work

--- 28.11 Optimization_Fcn.py
+++ 28.11 Optimization_Fcn.py
@@ -81,6 +81,16 @@ def ModelError(lambda_vals):
     return E 
 
 #%%
+from datetime import datetime
+
+# datetime object containing current date and time
+start = datetime.now()
+print(start)
+
 [lambda_vals,E] = findOptLambda()
 print(lambda_vals)
 
+finish = datetime.now()
+print(finish)
+
+print(finish-start)
