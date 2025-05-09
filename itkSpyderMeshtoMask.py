1a1f3bc54c6b6f7b7f2484c4ec560fb692b9df0b
Cassidy Northway <cbnorthway@gmail.com>
2025-05-09 14:24:07 -0700

    Modified and Corrected Files

--- itkSpyderMeshtoMask.py
+++ itkSpyderMeshtoMask.py
@@ -8,55 +8,62 @@ based on: https://github.com/InsightSoftwareConsortium/ITK/issues/2884
 
 import itk
 import os
-
+import tkinter
+from tkinter import filedialog
 
 #%%
 #Import mesh file
-path = os.getcwd() + '\\VesselFiles\\Utilized Subset\\dias_aorta.stl'
+path = os.getcwd() + '\\VesselFiles\\Utilized Subset\\Cleaned_dias_aorta.stl'
 
-#No idea what this does frankly
-TCoordinate = itk.F
-Dimension = 3
-TMesh = itk.Mesh[TCoordinate, Dimension].New()
-
-MeshType = itk.Mesh[itk.F, 3]
-reader = itk.MeshFileReader[MeshType].New()
-meshIO = itk.STLMeshIO.New()
-reader.SetMeshIO(meshIO)
-reader.SetFileName(path)
-reader.Update()
-mesh = reader.GetOutput()
-
-mesh_writer = itk.MeshFileWriter[TMesh].New()
-mesh_writer.SetFileName(os.getcwd() + "\\TempVTK\\Cleaned_Aorta.vtk")
-mesh_writer.SetInput(mesh)
-mesh_writer.Update()
-
-TMesh = itk.Mesh[itk.SS, Dimension].New()
-mesh_reader = itk.MeshFileReader[TMesh].New()
-mesh_reader.SetFileName(os.getcwd() + "\\TempVTK\\Cleaned_Aorta.vtk")
-mesh_reader.Update()
-mesh = mesh_reader.GetOutput()
-
-#%%
-TPixel = itk.SS                                                                     
-TImage = itk.Image[TPixel, Dimension]                                               
-                                                                                    
-image = itk.Image[TPixel, Dimension].New()                                          
-region = itk.ImageRegion[Dimension]()                                               
-region.SetSize([256, 256, 350])                                                     
-region.SetIndex([0, 0, 0])                                                          
-image.SetRegions(region)                                                            
-image.Allocate()                                                                    
-image.SetOrigin([-15, -30, -5])                                              
-image.SetSpacing([1, 1, 1])   
-
-#%%                                                  
-mesh_to_image_filter = itk.TriangleMeshToBinaryImageFilter[TMesh, TImage].New() 
-mesh_to_image_filter.SetInput(mesh)                                      
-mesh_to_image_filter.SetInfoImage(image)                                        
-mesh_to_image_filter.Update()
-filtered = mesh_to_image_filter.GetOutput()
-itk.imwrite(mesh_to_image_filter.GetOutput(), os.getcwd() + "\\VesselFiles\\Utilized Subset\\gzFiles\\cleaned.nii.gz") 
+tkinter.Tk().withdraw() # prevents an empty tkinter window from appearing
+file_names = filedialog.askopenfilenames()
+for file_name in file_names:
+    name = file_name.split('/')[-1]
+    name = name.split('.')[0]
+    
+    #No idea what this does frankly
+    TCoordinate = itk.F
+    Dimension = 3
+    TMesh = itk.Mesh[TCoordinate, Dimension].New()
+    
+    MeshType = itk.Mesh[itk.F, 3]
+    reader = itk.MeshFileReader[MeshType].New()
+    meshIO = itk.STLMeshIO.New()
+    reader.SetMeshIO(meshIO)
+    reader.SetFileName(file_name)
+    reader.Update()
+    mesh = reader.GetOutput()
+    
+    mesh_writer = itk.MeshFileWriter[TMesh].New()
+    mesh_writer.SetFileName(os.getcwd() + "\\TempVTK\\"+ name + ".vtk")
+    mesh_writer.SetInput(mesh)
+    mesh_writer.Update()
+    
+    TMesh = itk.Mesh[itk.SS, Dimension].New()
+    mesh_reader = itk.MeshFileReader[TMesh].New()
+    mesh_reader.SetFileName(os.getcwd() + "\\TempVTK\\" + name + ".vtk")
+    mesh_reader.Update()
+    mesh = mesh_reader.GetOutput()
+    
+    #%%
+    TPixel = itk.SS                                                                     
+    TImage = itk.Image[TPixel, Dimension]                                               
+                                                                                        
+    image = itk.Image[TPixel, Dimension].New()                                          
+    region = itk.ImageRegion[Dimension]()                                               
+    region.SetSize([256, 256, 350])                                                     
+    region.SetIndex([0, 0, 0])                                                          
+    image.SetRegions(region)                                                            
+    image.Allocate()                                                                    
+    image.SetOrigin([-15, -30, -5])                                              
+    image.SetSpacing([1, 1, 1])   
+    
+    #%%                                                  
+    mesh_to_image_filter = itk.TriangleMeshToBinaryImageFilter[TMesh, TImage].New() 
+    mesh_to_image_filter.SetInput(mesh)                                      
+    mesh_to_image_filter.SetInfoImage(image)                                        
+    mesh_to_image_filter.Update()
+    filtered = mesh_to_image_filter.GetOutput()
+    itk.imwrite(mesh_to_image_filter.GetOutput(), os.getcwd() + "\\VesselFiles\\Utilized Subset\\gzFiles\\" + name +".nii.gz") 
 
 #%%
