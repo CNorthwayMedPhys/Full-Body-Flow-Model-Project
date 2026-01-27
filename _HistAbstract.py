2d4fa0d5038ff37ed523d8aaba2b4f1be1985f42
Cassidy Northway <cbnorthway@gmail.com>
2026-01-27 13:14:59 -0800

    Finish comp abstract

--- _HistAbstract.py
+++ _HistAbstract.py
@@ -8,7 +8,7 @@ Created on Thu Jan 22 14:13:49 2026
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import gamma
+from scipy.stats import gamma, norm
 import matplotlib.lines as lines
 
 #%%Load in data
@@ -48,25 +48,40 @@ plt.show()
 
 print(f"Mean of dist: {mean:.2f}")
 print(f"Standard deviation of dist: {std:.2f}")
-# mean = np.mean(Frac1)
-# std_dev = np.std(Frac1)
 
-# plt.hist(Frac1, bins=50, alpha=0.7, color='skyblue', edgecolor='black', label='Blood Volume Dose')
+#%% All 6 fracs
+Frac1 = np.load("1FracAbstract.npy")
+Frac2 = np.load("1FracAbstract_2.npy")
+Frac3 = np.load("1FracAbstract_3.npy")
+Frac4 = np.load("1FracAbstract_4.npy")
+Frac5 = np.load("1FracAbstract_5.npy")
+Frac6 = np.load("1FracAbstract_6.npy")
 
-# # Add vertical lines for mean and standard deviation
-# plt.axvline(mean, color='red', linestyle='dashed', linewidth=2, label=f'Mean: {mean:.2E}')
-# plt.axvline(mean - std_dev, color='green', linestyle='dotted', linewidth=2, label=f'1 Std Dev: {std_dev:.2E}')
-# plt.axvline(mean + std_dev, color='green', linestyle='dotted', linewidth=2)
+FullFrac = Frac1[:,0] + Frac2[:,0] + Frac3[:,0] + Frac4[:,0] + Frac5[:,0] + Frac6[:,0]
 
-# # Add labels and title
-# plt.xlabel('Dose (Gy)')
-# plt.ylabel('Blood Volume Count')
-# plt.title('Single Fracation Results for Co-60 Sweeping TBI')
-# plt.legend()
-# plt.grid(axis='y', alpha=0.75)
+# Fit the Gamma distribution to the data
+mu, std = norm.fit(FullFrac)
 
-# # Display the plot
-# plt.show()
+# Create a range of x-values for the plot
+x = np.linspace(0, 20, 1000)
+pdf_fitted = norm.pdf(x, mu, std)
+
+# Plot histogram and fitted curve
+plt.hist(FullFrac, bins=30, density=True, alpha=0.5, label='Dose to Blood Volumes')
+
+plt.plot(x, pdf_fitted, 'r-', label='Fitted Norm PDF')
+plt.axvline(mu, color='black', linestyle='dashed', linewidth=2, label=f'Mean: {mu:.2f} Gy')
+plt.title('Dose to Circulating Blood Volumes after All Fractions')
+plt.xlabel('Dose to Blood Volume (Gy)')
+plt.ylabel('Density')
+plt.legend()
 
 
+handles, labels = plt.gca().get_legend_handles_labels()
+proxy_patch = lines.Line2D([0],[0], label= f'Std: {std:.2f} Gy', color = "white")
+handles.append(proxy_patch)
+plt.legend(handles=handles, labels=[h.get_label() for h in handles])
+plt.show()
 
+print(f"Mean of dist: {mu:.2f}")
+print(f"Standard deviation of dist: {std:.2f}")
